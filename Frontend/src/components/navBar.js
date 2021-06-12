105c579bd72b957ee34d01495944ab87dd2a4e1e
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-06-12 17:37:03 +0530

    Add material design attributes to navBar

--- Frontend/src/components/navBar.js
+++ Frontend/src/components/navBar.js
@@ -10,8 +10,9 @@ class NavBar extends Component {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Sample</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" 
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="fas fa-bars"></i>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
